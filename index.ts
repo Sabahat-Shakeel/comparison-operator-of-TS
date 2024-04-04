3240f3ccbd01c767dc679b75940200b3254583ad
syeda <sbtshakeel61@gmail.com>
2024-04-04 05:31:16 +0500

    push my comparison operator, TS

---
+++ index.ts
@@ -0,0 +1,16 @@
+// comparison operators
+
+let a = 5 
+let b = 4
+
+let isEqual = (a === b) // false
+console.log (isEqual)
+
+let isNotEqual = ( a != b)
+console.log(isNotEqual)  // true
+
+let isGreaterThan = (a > b) // true
+console.log(isGreaterThan)
+
+let isLessThan = (a < b)
+console.log(isLessThan) //false
